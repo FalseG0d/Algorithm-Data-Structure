29510dc2fea0dfac851c12ab84757575d19a53e1
Apoorv Garg <u.garg.10@gmail.com>
2020-12-22 21:16:15 +0530

    22 Dec

---
+++ Hackerrank/ViralAdvertising.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the viralAdvertising function below.
+int viralAdvertising(int n) {
+    int s=0;
+    int x=5;
+    
+    for(int i=0;i<n;i++){
+        s+=x/2;
+        x/=2;
+        x*=3;
+    }
+    
+    return s;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int result = viralAdvertising(n);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
