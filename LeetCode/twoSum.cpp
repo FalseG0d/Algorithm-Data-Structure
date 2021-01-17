9e426030b868c77acc480cdeab07bebe94bd2c43
Apoorv Garg <u.garg.10@gmail.com>
2021-01-17 21:50:35 +0530

    Create twoSum.cpp

---
+++ LeetCode/twoSum.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> bruteTwoSum(vector<int>& nums, int target) {
+        vector<int> temp;
+        
+        for(int i=0;i<nums.size()-1;i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]+nums[j]==target){
+                    temp.push_back(i);
+                    temp.push_back(j);
+                    cout<<i<<" "<<j;
+                    break;
+                }
+            }
+        }
+        
+        return temp;
+    }
+	vector<int> optimizedTwoSum(vector<int>& nums, int target) {
+        map<int,int> maps;
+        vector<int> temp;
+        for(int i=0;i<nums.size();i++){
+            maps[nums[i]]=i;
+        }
+        for(int i=0;i<nums.size();i++){
+            int complement=target-nums[i];
+            if(maps[complement]&&maps[complement]!=i){
+                temp.push_back(i);
+                temp.push_back(maps[complement]);
+                return temp;
+            }
+        }
+    return temp;
+    }
+};
