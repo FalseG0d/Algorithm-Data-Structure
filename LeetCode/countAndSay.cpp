d9fa4f82306292c21aa7134de8cce4dadf0ef906
Apoorv Garg <u.garg.10@gmail.com>
2021-02-19 19:43:12 +0530

    New Question

---
+++ LeetCode/countAndSay.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        if(n==1){
+            return "1";
+        }
+        
+        string word=countAndSay(n-1);
+        char itr=word[0];
+        int count=0;
+        string res;
+        
+        for(int i=0;word[i]!='\0';i++){
+            if(word[i]!=itr){
+                stringstream ss;
+                ss<<count;
+                res+=ss.str();
+                res+=itr;
+                itr=word[i];
+                count=0;
+                i--;
+            }else{
+                count++;
+            }
+        }
+        stringstream ss;
+        ss<<count;
+        res+=ss.str();
+        res+=itr;
+        
+        return res;
+    }
+};
