c60cdd4e7a6cfa09a01f2fc52678a73ec61f84cd
Apoorv Garg <u.garg.10@gmail.com>
2021-02-25 21:31:58 +0530

    New Question

--- General/Misc/stringSubset.cpp
+++ General/Misc/stringSubset.cpp
@@ -1,27 +1,24 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-void subString(char str[],int l,int r){
-    if(l>r){
-        for(int i=l;i<r;i++)cout<<str[i];
-        cout<<"\n";
-    }
-
-    subString(str,l+1,r);
-    subString(str,l,r-1);
+void printSubsequence(string input, string output)
+{
+    if (input.empty()) {
+		cout << output << endl;
+		return;
+	}
+    
+	printSubsequence(input.substr(1), output + input[0]);
+    
+	printSubsequence(input.substr(1), output);
 }
 
-int main() {
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
+int main()
+{
+	string output = "";
+	string input = "abcd";
 
-        char*str=new char[n];
-        for(int i=0;i<n;i++)cin>>str[i];
-        
-        subString(str,0,n-1);
-    }
-    return 0;
+	printSubsequence(input, output);
+
+	return 0;
 }
