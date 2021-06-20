5f569033759b429b8bd6d2c3af9c6de9f92d684f
Apoorv Garg <u.garg.10@gmail.com>
2021-06-20 21:05:22 +0530

    Question

--- GeeksForGeeks/PractiseChallege/reverseGrouupedLL.cpp
+++ GeeksForGeeks/PractiseChallege/reverseGrouupedLL.cpp
@@ -1,26 +1,130 @@
-struct node *reverse (struct node *head, int k)
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+struct node
 {
+    int data;
+    struct node* next;
+    
+    node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
 
-int c=0;
-struct node *parso=NULL;
-struct node *aaj=head;
-struct node *kal=NULL;
-
-while(aaj!=NULL && c<k) {="" kal="aaj-">next;
-aaj->next=parso;
-parso=aaj;
-aaj=kal;
-
-c++;
-
-}
-
-if(kal!=NULL)
+/* Function to print linked list */
+void printList(struct node *node)
 {
-
-head->next=reverse(kal, k);
-
+    while (node != NULL)
+    {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
 }
 
-return parso;
+
+ // } Driver Code Ends
+/*
+  Reverse a linked list
+  The input list will have at least one element  
+  Return the node which points to the head of the new LinkedList
+  Node is defined as 
+    struct node
+    {
+        int data;
+        struct node* next;
+    
+        node(int x){
+            data = x;
+            next = NULL;
+        }
+    
+    }*head;
+*/
+
+class Solution
+{
+    struct node*ret=NULL;
+    struct node*retItr=NULL;
+    public:
+    struct node *push(struct node*head,int itr,int k){
+        if(head->next==NULL||itr==k-1){
+            if(ret==NULL){
+                ret=new node(head->data);
+                retItr=ret;
+            }
+            else {
+                retItr->next=new node(head->data);
+                retItr=retItr->next;
+            }
+              
+            return head->next;
+        }
+        
+        struct node*tmpRet=push(head->next,itr+1,k);
+        
+        retItr->next=new node(head->data);
+        retItr=retItr->next;
+        
+        
+        return tmpRet;
+    }
+    struct node *reverse (struct node *head, int k)
+    { 
+        // Complete this method
+        struct node*itr=push(head,0,k);
+        
+        while(itr!=NULL)itr=push(itr,0,k);
+        
+        return ret;
+    }
+};
+
+
+// { Driver Code Starts.
+
+/* Drier program to test above function*/
+int main(void)
+{
+    int t;
+    cin>>t;
+     
+    while(t--)
+    {
+        struct node* head = NULL;
+        struct node* temp = NULL;
+        int n;
+        cin >> n;
+         
+        for(int i=0 ; i<n ; i++)
+        {
+            int value;
+            cin >> value;
+            if(i == 0)
+            {
+                head = new node(value);
+                temp = head;
+            }
+            else
+            {
+                temp->next = new node(value);
+                temp = temp->next;
+            }
+        }
+        
+        int k;
+        cin>>k;
+        
+        Solution ob;
+        head = ob.reverse(head, k);
+        printList(head);
+    }
+     
+    return(0);
 }
+
+  // } Driver Code Ends
