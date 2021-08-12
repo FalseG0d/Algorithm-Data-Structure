55968d04635d6c7e6469f66aad428ba5feb77e8d
Apoorv Garg <u.garg.10@gmail.com>
2021-08-12 20:52:14 +0530

    TechDose

--- TechDose/InversionCount.cpp
+++ TechDose/InversionCount.cpp
@@ -6,9 +6,6 @@ using namespace std;
  // } Driver Code Ends
 class Solution{
   public:
-    // arr[]: Input Array
-    // N : Size of the Array arr[]
-    // Function to count inversions in the array.
     long long merge(long long arr[], long long low, long long mid, long long high)
     {
         long long n1 = mid - low + 1;
