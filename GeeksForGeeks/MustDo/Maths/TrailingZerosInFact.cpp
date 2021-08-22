8c77a6dc09009e8f61790a4ad2781bc139f0c49b
Apoorv Garg <u.garg.10@gmail.com>
2021-08-22 20:00:17 +0530

    Must Do

---
+++ GeeksForGeeks/MustDo/Maths/TrailingZerosInFact.cpp
@@ -0,0 +1,39 @@
+// { Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+ // } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+public:
+    int trailingZeroes(int N)
+    {
+        // Write Your Code here
+        int temp = 5;
+        int res = 0;
+        
+        while(N/temp > 0){
+            res += N/temp;
+            temp *= 5;
+        }
+        
+        return res;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin >> N;
+        Solution ob;
+        int ans  = ob.trailingZeroes(N);
+        cout<<ans<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
