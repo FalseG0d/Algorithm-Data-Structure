687c5137add9e733825b6b0755a8e7317879781b
Apoorv Garg <u.garg.10@gmail.com>
2021-09-18 22:19:58 +0530

    Leetcode

---
+++ LeetCode/Medium/Combinations.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> res;
+    
+    void help(int beg, const int& end, int k, vector<int> temp){        
+        if(k == 0){
+            res.push_back(temp);
+            return;
+        }
+        
+        for(int i=beg;i<=end;i++){
+            temp.push_back(i);
+            help(i+1,end,k-1,temp);
+            temp.pop_back();
+        }
+    }
+    
+    vector<vector<int>> combine(int n, int k) {
+        help(1, n, k, {});
+        
+        return res;
+    }
+};
---
+++ LeetCode/Medium/WordSearch.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int M = 0;
+    int N = 0;
+    bool** visited;
+    
+    bool help(int i, int j, int curr, string &word, vector<vector<char>>& board) {
+        if(curr == word.length()) return true;
+        if(i >= M || j >= N || i < 0 || j < 0) return false;
+        
+        if(visited[i][j]) return false;
+        if(word[curr] != board[i][j]) return false;
+        
+        visited[i][j] = true;
+        
+        // cout<<word[curr]<<" "<<i<<" "<<j<<"\n";
+        
+        bool up     = help(i - 1, j, curr + 1, word, board);
+        bool down   = help(i + 1, j, curr + 1, word, board);
+        bool left   = help(i, j - 1, curr + 1, word, board);
+        bool right  = help(i, j + 1, curr + 1, word, board);
+        
+        visited[i][j] = false;
+        
+        return up || down || left || right;
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        M = board.size();
+        N = board[0].size();
+        
+        visited = new bool*[M];
+        for(int i=0;i<M;i++) visited[i] = new bool[N]{false};
+        
+        bool res = false;
+        
+        for(int i=0;i<M;i++){
+            for(int j=0;j<N;j++){
+                if(res) return true;
+                
+                res = help(i, j, 0, word, board);
+            }
+        }
+        
+        return res;
+    }
+};
