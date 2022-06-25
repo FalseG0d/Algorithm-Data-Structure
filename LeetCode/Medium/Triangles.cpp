dff56f1db603a7ad14d0ce8bd623dea1f4f0f9b9
FalseG0d <garg.apoorv1098@gmail.com>
2022-06-25 21:47:10 +0530

    Minor

--- LeetCode/Medium/Triangles.cpp
+++ LeetCode/Medium/Triangles.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int levels = triangle.size();
+        
         for(int i=1;i<levels;i++){
             for(int j=0;j<i+1;j++){
                 if(j == 0) triangle[i][j] += triangle[i-1][j];
@@ -9,10 +10,12 @@ public:
                 else triangle[i][j] += min(triangle[i-1][j], triangle[i-1][j-1]);
             }
         }
+        
         int res = INT_MAX;
         for(int x:triangle[levels-1]){
             res = min(res, x);
         }
+        
         return res;
     }
 };
