f4f2c556636d4067cdf283c02163ddd9e37721a5
FalseG0d <garg.apoorv1098@gmail.com>
2022-08-05 22:36:08 +0530

    Minor

--- LeetCode/Easy/factTrailingZeros.cpp
+++ LeetCode/Easy/factTrailingZeros.cpp
@@ -3,12 +3,10 @@ public:
     int trailingZeroes(int n) {
         int x=5;
         int res=0;
-        
         while(x<=n){
             res+=n/x;
             x*=5;
         }
-        
         return res;
     }
 };
