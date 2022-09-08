dd0afc340a8fe7bf0679ddfccbed5524da5fcef2
Apoorv Garg <u.garg.10@gmail.com>
2022-09-08 21:22:29 +0530

    Minot

---
+++ LeetCode/Medium/CountPrimes.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        if(n == 0 || n == 1 || n == 2) return 0;
+        
+        int res = 0;
+        
+        vector<bool>mp(n, false);
+        
+        mp[0] = mp[1] = true;
+        
+        for(long long i=2;i<n;i++){
+            if(mp[i]) continue;
+            
+            res++;
+            
+            long long j = 0;
+            for(long long j=i*i;j<n;j+=i){
+                mp[j] = true;
+            }
+        }
+        
+        return res;
+    }
+};
