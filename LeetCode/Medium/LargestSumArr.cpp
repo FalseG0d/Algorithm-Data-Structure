6603c8171d62077f0ce257bb0c07054b14d2c8fa
FalseGodGames <garg.apoorv1011@gmail.com>
2022-09-28 22:02:22 +0530

    Minor

---
+++ LeetCode/Medium/LargestSumArr.cpp
@@ -0,0 +1,21 @@
+class Solution{
+    public:
+    int maxLen(vector<int>&A, int n)
+    {   
+        // Your code here
+        unordered_map<int,int>mp;
+        int sum = 0;
+        int res = 0;
+        
+        mp[0] = -1;
+        
+        for(int i=0;i<n;i++){
+            sum += A[i];
+            if(mp.find(sum) != mp.end()) res = max(res, i - mp[sum]);
+            else mp[sum] = i;
+        }
+        
+        
+        return res;
+    }
+};
