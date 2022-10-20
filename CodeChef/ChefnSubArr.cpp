b5541a3d7001c0c66fb904bb25df53d83f7f474a
FalseGodGames <garg.apoorv1098@gmail.com>
2022-10-20 20:46:56 +0530

    Create ChefnSubArr.cpp

---
+++ CodeChef/ChefnSubArr.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	
+	while(T--){
+	    int N;
+	    cin>>N;
+	    
+	    std::vector<int> v(N);
+	    for(int i=0;i<N;i++) cin>>v[i];
+	    
+	    int**arr1 = new int*[N];
+	    int**arr2 = new int*[N];
+	    
+	    for(int i=0;i<N;i++){
+	        arr1[i] = new int[N];
+	        arr2[i] = new int[N];
+	    }
+	    
+	    int res = 0;
+	   
+	   for(int i=0;i<N;i++){
+	       arr1[i][i] = v[i];
+	       arr2[i][i] = v[i];
+	   }
+	   
+	   for(int i=0;i<N;i++){
+	       for(int j=i+1;j<N;j++){
+	           arr1[i][j] = arr1[i][j-1] + v[j];
+	           arr2[i][j] = arr2[i][j-1] * v[j];
+	       }
+	   }
+	   
+	   //for(int i=0;i<N;i++){
+	   //    for(int j=0;j<N;j++) cout<<arr1[i][j]<<" ";
+	   //    cout<<"\n";
+	   //}
+	   
+	   //cout<<"\n";
+	   
+	   //for(int i=0;i<N;i++){
+	   //    for(int j=0;j<N;j++) cout<<arr2[i][j]<<" ";
+	   //    cout<<"\n";
+	   //}
+	   
+	   for(int i=0;i<N;i++){
+	       for(int j=i;j<N;j++){
+	           if(arr1[i][j] == arr2[i][j]) res++;
+	       }
+	   }
+	   
+	   
+	   cout<<res<<"\n";
+	}
+	
+	return 0;
+}
