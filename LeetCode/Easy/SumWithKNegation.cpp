58291cbdbde6287a09116aee938e4d3132136fc5
Apoorv Garg <garg.apoorv1098@gmail.com>
2023-02-15 20:51:27 +0530

    New Question

---
+++ LeetCode/Easy/SumWithKNegation.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int largestSumAfterKNegations(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int i = 0;
+        while(i<nums.size() && nums[i]<0 && k>0)
+        {
+            nums[i] *= -1;
+            i++;
+            k--;
+        }
+        if(k%2 == 1)
+        {
+            sort(nums.begin(),nums.end());
+            nums[0] *= -1;
+        }
+        int sum = 0;
+        for(int n : nums)
+        {
+            sum += n;
+        }
+        return sum;
+    }
+};
