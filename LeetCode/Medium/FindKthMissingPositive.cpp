7347393085abdea798585d5b3ece93e49664b4b2
Apoorv Garg <garg.apoorv1098@gmail.com>
2023-03-06 23:35:35 +0530

    New Question

---
+++ LeetCode/Medium/FindKthMissingPositive.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        // for(int ele : arr) cout<<ele<<" ";
+        // cout<<"\n";
+        
+        int N = arr.size();
+        
+        int itr = 0;
+        int val = 1;
+
+        while(itr < N) {
+            // cout<<arr[itr]<<" "<<val<<", ";
+            if(val == arr[itr]) itr++;
+            else {
+                k--;
+
+                if(k == 0) break;
+                }
+
+            val++;
+        }
+
+        if(k > 0) return val + k - 1;
+
+        return val;
+    }
+};
