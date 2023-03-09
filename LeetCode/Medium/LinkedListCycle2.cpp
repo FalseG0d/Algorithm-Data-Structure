d67870f237734d5d9d5f82cfdc8723ff7ff49e35
Apoorv Garg <garg.apoorv1098@gmail.com>
2023-03-09 21:12:08 +0530

    Minor

---
+++ LeetCode/Medium/LinkedListCycle2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode *fast = head;
+        ListNode *slow = head;
+        while(fast != nullptr && fast->next != nullptr) {
+            fast = fast->next->next;
+            slow = slow->next;
+            if (fast == slow) {
+                slow = head;
+                while(slow != fast) {
+                    fast = fast->next;
+                    slow = slow->next;
+                }
+                return slow;
+            }
+        }
+        return nullptr;
+    }
+};
