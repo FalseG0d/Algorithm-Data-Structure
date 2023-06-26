e4bb481d11cdae52d5923b5ef79327dcd1ed7db7
Apoorv Garg <garg.apoorv1098@gmail.com>
2023-06-26 21:54:37 +0530

    Minor

--- LeetCode/Easy/optimizedLLIntersection.cpp
+++ LeetCode/Easy/optimizedLLIntersection.cpp
@@ -3,20 +3,25 @@ public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         int l1 =0;
         int l2=0;
+
         ListNode *temp1 = headA;
         ListNode *temp2 = headB;
+        
         while(headA!=NULL)
         {
             l1++;
             headA = headA->next;
         }
+        
         while(headB!=NULL)
         {
             l2++;
             headB = headB->next;
         }
+        
         headA = temp1;
         headB = temp2;
+        
         if(l1>l2)
         {
             int space =  l1-l2;
@@ -35,6 +40,7 @@ public:
                 space--;
             }
         }
+        
         while(headA!=NULL && headB!=NULL)
         {
             if(headA==headB)
