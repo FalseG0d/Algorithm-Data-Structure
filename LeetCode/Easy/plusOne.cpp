5bd65b4733fcb5d3e699eb604e2c810e8e9f2295
Apoorv Garg <garg.apoorv1098@gmail.com>
2023-07-15 21:07:43 +0530

    Minor

--- LeetCode/Easy/plusOne.cpp
+++ LeetCode/Easy/plusOne.cpp
@@ -7,11 +7,13 @@ public:
         for(int i=digits.size()-1;i>=0;i--){
             digits[i]+=carry;
             carry=digits[i]/10;
+
             digits[i]%=10;
         }
         
         if(carry>0){
             digits.push_back(0);
+            
             for(int i=digits.size()-2;i>=0;i--){
                 digits[i+1]=digits[i];
             }
