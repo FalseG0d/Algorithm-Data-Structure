f7d4521cabe91a1bf3c01532e96f108d8e5e1dca
Apoorv Garg <garg.apoorv1098@gmail.com>
2023-07-16 22:47:04 +0530

    Minor

--- LeetCode/Easy/revInteger.cpp
+++ LeetCode/Easy/revInteger.cpp
@@ -6,10 +6,13 @@ public:
         while(x!=0){
             int pop=x%10;
             x/=10;
+            
             if((sum>INT_MAX/10||sum==INT_MAX/10&&pop>7)||(sum<INT_MIN/10||sum==INT_MIN/10&&pop<-8))return 0;
+            
             sum*=10;
             sum+=pop;
         }
+        
         return sum;
     }
 };
