4e0df77e7d6231db6a8632d2c3ec6a327d6a7203
Apoorv Garg <garg.apoorv1098@gmail.com>
2023-08-17 22:41:45 +0530

    Minor

--- LeetCode/Medium/SimplifyPath.cpp
+++ LeetCode/Medium/SimplifyPath.cpp
@@ -2,12 +2,13 @@ class Solution {
 public:
     string simplifyPath(string path) {
         int N = path.length();
+        
         for(int i=0;i<N;i++) if(path[i] == '/') path[i] = ' ';
         
         stringstream ss(path);
         string str="";
-        
         vector<string>tree;
+        
         while(ss>>str){
             if(str == ".." && tree.size() > 0) tree.pop_back();
             else if(str == "." || str == "..") continue;
@@ -16,8 +17,8 @@ public:
         
         string res = "/";
         auto itr = tree.begin();
-        
         N = tree.size();
+        
         for(int i=0;i<N;i++){
             res += tree[i];
             if(i < N - 1) res += "/";
