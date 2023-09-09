0ad3c6d9ca9ba8465c8ff79967d2c461dc9a118e
Apoorv Garg <garg.apoorv1098@gmail.com>
2023-09-09 23:13:12 +0530

    Minor

--- LeetCode/Medium/MergeInterval.cpp
+++ LeetCode/Medium/MergeInterval.cpp
@@ -5,9 +5,11 @@ public:
         int N = intervals.size();
         
         int i = 0;
+
         while(i < N && intervals[i][1] < newInterval[0]) res.push_back(intervals[i++]);
         
         vector<int> mergedInterval = newInterval;
+        
         while(i < N && intervals[i][0] <= newInterval[1]){
             mergedInterval[0] = min(mergedInterval[0], intervals[i][0]);
             mergedInterval[1] = max(mergedInterval[1], intervals[i++][1]);
