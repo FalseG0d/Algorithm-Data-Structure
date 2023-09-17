50da8d28a63d3e2969ee6ce12100211e147fe5b4
Apoorv Garg <garg.apoorv1098@gmail.com>
2023-09-17 22:04:53 +0530

    Minor

--- LeetCode/Medium/RotateMatrix.cpp
+++ LeetCode/Medium/RotateMatrix.cpp
@@ -4,18 +4,26 @@ public:
         int N = matrix.size();
         int**mat = new int*[N];
 
-        for(int i=0;i<N;i++) mat[i] = new int[N];
-        
-        for(int i=0;i<N;i++){
-            for(int j=0;j<N;j++) mat[j][i] = matrix[i][j];
+        for(int i=0;i<N;i++) {
+            mat[i] = new int[N];
         }
         
         for(int i=0;i<N;i++){
-            for(int j=0;j<N/2;j++) swap(mat[i][j], mat[i][N - j - 1]);
+            for(int j=0;j<N;j++) {
+                mat[j][i] = matrix[i][j];
+                }
         }
         
         for(int i=0;i<N;i++){
-            for(int j=0;j<N;j++) matrix[i][j] = mat[i][j];
+            for(int j=0;j<N/2;j++) {
+                swap(mat[i][j], mat[i][N - j - 1]);
+                }
+        }
+        
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++) {
+                matrix[i][j] = mat[i][j];
+                }
         }
     }
 };
