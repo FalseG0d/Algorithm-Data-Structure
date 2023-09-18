358dd300f19538887f1d687d943062b68e0c5823
Apoorv Garg <garg.apoorv1098@gmail.com>
2023-09-18 22:55:28 +0530

    Minor

--- LeetCode/Medium/SpiralMatrix.cpp
+++ LeetCode/Medium/SpiralMatrix.cpp
@@ -19,15 +19,18 @@ public:
                 for(int i=colBeg;i<=colEnd;i++) res.push_back(matrix[rowBeg][i]);
                 
                 rowBeg++;
-            }else if(dir == 1){
+            }
+            else if(dir == 1){
                 for(int i=rowBeg;i<=rowEnd;i++) res.push_back(matrix[i][colEnd]);
                 
                 colEnd--;
-            }else if(dir == 2){
+            }
+            else if(dir == 2){
                 for(int i=colEnd;i>=colBeg;i--) res.push_back(matrix[rowEnd][i]);
                 
                 rowEnd--;
-            }else{
+            }
+            else{
                 for(int i=rowEnd;i>=rowBeg;i--) res.push_back(matrix[i][colBeg]);
                 
                 colBeg++;
