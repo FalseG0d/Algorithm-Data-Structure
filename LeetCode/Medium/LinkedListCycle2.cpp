7cefeb6e0171b168bbfa9994cc040d16303aeb58
Apoorv Garg <garg.apoorv1098@gmail.com>
2023-10-05 22:15:56 +0530

    Minor

--- LeetCode/Medium/LinkedListCycle2.cpp
+++ LeetCode/Medium/LinkedListCycle2.cpp
@@ -3,18 +3,23 @@ public:
     ListNode *detectCycle(ListNode *head) {
         ListNode *fast = head;
         ListNode *slow = head;
+
         while(fast != nullptr && fast->next != nullptr) {
             fast = fast->next->next;
             slow = slow->next;
+            
             if (fast == slow) {
                 slow = head;
+                
                 while(slow != fast) {
                     fast = fast->next;
                     slow = slow->next;
                 }
+                
                 return slow;
             }
         }
+        
         return nullptr;
     }
 };
