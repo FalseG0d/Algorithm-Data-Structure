12a89159eafba33e98a75576748807b786a675c9
Apoorv Garg <garg.apoorv1098@gmail.com>
2023-10-07 22:39:16 +0530

    Minor

--- LeetCode/Medium/maxWater.cpp
+++ LeetCode/Medium/maxWater.cpp
@@ -4,10 +4,11 @@ public:
         int lm {0};
         int rm = height.size()-1;
         int maxArea {0};
-        
+
         while (lm < rm) {
             maxArea = std::max(maxArea, std::min(height[lm], height[rm]) * (rm - lm));
-            (height[lm]<=height[rm])?lm++:rm--;
+
+            (height[lm] <= height[rm]) ? lm++ : rm--;
         }
         
         return maxArea;
