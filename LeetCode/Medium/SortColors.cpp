6ecf38789f77da190a9523903a711d2de030fb14
Apoorv Garg <garg.apoorv1098@gmail.com>
2023-10-28 21:58:55 +0530

    Minor

--- LeetCode/Medium/SortColors.cpp
+++ LeetCode/Medium/SortColors.cpp
@@ -2,12 +2,16 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         vector<int>colors(3,0);
+        
         for(int ele : nums) colors[ele]++;
+        
         int itr = 0;
         int N = nums.size();
         int curr = 0;
+        
         while(itr < N){
             while(curr < 3 && colors[curr] == 0) curr++;
+            
             nums[itr++] = curr;
             colors[curr]--;
         }
