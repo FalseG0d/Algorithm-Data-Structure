c535427e57d034421553b641d3730176b1e24020
Apoorv Garg <garg.apoorv1098@gmail.com>
2023-11-10 23:03:15 +0530

    Minor

--- LeetCode/Easy/PowerOfThree.cpp
+++ LeetCode/Easy/PowerOfThree.cpp
@@ -3,8 +3,10 @@ public:
     bool isPowerOfThree(int n) {
         if (n <= 0)
             return false;
+
         while (n % 3 == 0)
             n /= 3;
+        
         return (n == 1);
     }
 };
