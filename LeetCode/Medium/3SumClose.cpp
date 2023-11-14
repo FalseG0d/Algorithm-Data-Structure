f07e099b9c5a4f13278412374184c382a8196162
Apoorv Garg <garg.apoorv1098@gmail.com>
2023-11-14 22:05:43 +0530

    Minor

---
+++ LeetCode/Medium/3SumClose.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        int N = nums.size();
+        int ret = nums[0] + nums[1] + nums[2];
+        
+        sort(nums.begin(), nums.end());
+
+        for(int i=0;i<N;i++) {
+            int to_make = target - nums[i];
+
+            int beg = 0;
+            int end = N - 1;
+
+            while(beg < end) {
+                if(beg == i) {
+                    beg++;
+                    continue;
+                    }
+                if(end == i) {
+                    end--;
+                    continue;
+                    }
+
+                if(abs(target - (nums[beg] + nums[end] + nums[i])) < abs(target - ret)) {
+                    ret = nums[beg] + nums[end] + nums[i];
+                }
+
+                if(nums[beg] + nums[end] == to_make) break;
+                else if(nums[beg] + nums[end] > to_make) end--;
+                else beg++;
+            }
+        }
+
+        return ret;
+    }
+};
