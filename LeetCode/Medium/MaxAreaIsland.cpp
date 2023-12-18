95df0b502e24da5485a385e1e5bab0ae519a8edd
Apoorv Garg <garg.apoorv1098@gmail.com>
2023-12-18 22:23:16 +0530

    Minor

--- LeetCode/Medium/MaxAreaIsland.cpp
+++ LeetCode/Medium/MaxAreaIsland.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int areaDFS(vector<vector<int>>& grid, int i, int j, int M, int N){
         if(i < 0 || j < 0 || i == M || j == N) return 0;
+        
         if(grid[i][j] == 0) return 0;
         
         grid[i][j] = 0;
