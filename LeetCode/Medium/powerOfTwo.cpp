79949019af07ab06c82c370d0e3c45b48ab4b21a
Apoorv Garg <garg.apoorv1098@gmail.com>
2023-12-21 22:13:09 +0530

    Minor

--- LeetCode/Medium/powerOfTwo.cpp
+++ LeetCode/Medium/powerOfTwo.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
         if(n<=0)return false;
+        
         n=n&(n-1);
         
         if(n==0)return true;
         else return false;
-            
     }
 };
