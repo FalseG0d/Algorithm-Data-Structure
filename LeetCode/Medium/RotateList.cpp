6634a20bdf13b4f4908be471e9eb8bd157c4eb65
Apoorv Garg <garg.apoorv1098@gmail.com>
2024-01-21 22:46:16 +0530

    Minor

--- LeetCode/Medium/RotateList.cpp
+++ LeetCode/Medium/RotateList.cpp
@@ -48,6 +48,7 @@ public:
         if(val == -1000) return head;
         
         ListNode* newHead = new ListNode(val);
+        
         newHead -> next = head;
         
         return help(newHead, k - 1);
