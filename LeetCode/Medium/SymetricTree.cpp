0109d18d3fa18ce0b42392ff4bac8bcce2377cbc
Apoorv Garg <garg.apoorv1098@gmail.com>
2024-03-17 22:39:39 +0530

    Minor

--- LeetCode/Medium/SymetricTree.cpp
+++ LeetCode/Medium/SymetricTree.cpp
@@ -12,15 +12,15 @@
 class Solution {
 public:
     bool isMirror(TreeNode*left,TreeNode*right){
-        if(left==NULL&&right==NULL)return true;
-        if((left==NULL&&right!=NULL)||(left!=NULL&&right==NULL))return false;
-        if(left->val!=right->val)return false;
+        if(left==NULL&&right==NULL) return true;
+        if((left==NULL&&right!=NULL)||(left!=NULL&&right==NULL)) return false;
+        if(left->val!=right->val) return false;
         
         return isMirror(left->left,right->right)&&isMirror(left->right,right->left);
     }
     
     bool isSymmetric(TreeNode* root) {
-        if(root==NULL)return true;
+        if(root==NULL) return true;
         
         TreeNode*left=root->left;
         TreeNode*right=root->right;
