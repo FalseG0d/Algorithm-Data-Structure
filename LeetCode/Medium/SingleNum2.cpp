947d8678869334a45f8636c765fa7456ebc05520
FalseG0d <garg.apoorv1098@gmail.com>
2024-08-22 19:58:25 +0530

    New Code

---
+++ LeetCode/Medium/SingleNum2.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int itr = 1;
+        int res = 0;
+
+        for(int i=0;i<32;i++) {
+            int cnt = 0;
+
+            for(int ele: nums) {
+                cnt += (ele >> i) & 1 > 0;
+            }
+
+            if(cnt % 3 == 1) res += (1 << i);
+        }
+
+        return res;
+    }
+};
