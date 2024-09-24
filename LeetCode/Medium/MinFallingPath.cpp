e68465eb434f871b6399586c8bef879847cb9921
FalseG0d <garg.apoorv1098@gmail.com>
2024-09-24 21:25:39 +0530

    New Code

---
+++ LeetCode/Medium/MinFallingPath.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int N = matrix.size();
+
+        for(int i=N-2;i>=0;i--) {
+            for(int j=0;j<N;j++) {
+                int left = INT_MAX;
+                int right = INT_MAX;
+                int center = INT_MAX;
+
+                if(j > 0) left = matrix[i+1][j-1];
+                if(j < N-1) right = matrix[i+1][j+1];
+                center = matrix[i+1][j];
+
+                matrix[i][j] += min(left, min(center, right));
+            }
+        }
+
+        int res = INT_MAX;
+        for(int i=0;i<N;i++) res = min(res, matrix[0][i]);
+
+        return res;
+    }
+};
